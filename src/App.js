c5b5a04f76302e797c0f7f5c6309de4d20e676de
avelyno <kou2295@calu.edu>
2021-01-15 17:35:05 -0500

    birthday reminder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import data from './data'
 
 function App() {
+  const [friends, setFriends] = useState(data)
+  function handleDelete(){
+    setFriends([])
+  }
+  function handleEachDelete(id){
+    const updatedList = friends.filter(friend => friend.id !== id)
+    setFriends(updatedList)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='card'>
+        <h3>{friends.length} Birthdays today!</h3>
+        {friends.map((friend) => {
+          return(
+            <article key={friend.id} className='friend'>
+              <img src={friend.image}/>
+              <div>
+                <button type='button' className='btn1' onClick={() => handleEachDelete(friend.id)}>X</button>
+                <h4>{friend.name}</h4> 
+                <p>{friend.age}</p>
+              </div>
+            </article>
+            )
+          })
+        }
+        <button type='button' className='btn2' onClick={handleDelete}>Clear</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
+
